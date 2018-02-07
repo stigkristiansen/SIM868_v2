bc85778184c735b914e405587dafddf5141e4eee
stigkristiansen <stigkristiansen@outlook.com>
2018-02-07 15:24:16 +0100

    .

--- libs/_Dispatch.php
+++ libs/_Dispatch.php
@@ -4,7 +4,7 @@ $moduleInstanceId = $_IPS['InstanceId'];
 $message = $_IPS['Message'];
 $log = $_IPS['Log'];
 
-$moduleName = IPS_GetObject(moduleInstanceId)['ObjectName'];
+$moduleName = IPS_GetObject($moduleInstanceId)['ObjectName'];
 
 if(preg_match_all('/\+CMTI: \"(SM|ME)\",([0-9]+)$/', $arr[$i], $matches, PREG_SET_ORDER, 0)!=0) {
 	IPS_LogMessage(" Processing incoming message...");
@@ -12,9 +12,9 @@ if(preg_match_all('/\+CMTI: \"(SM|ME)\",([0-9]+)$/', $arr[$i], $matches, PREG_SE
 	$deleteCommand = "AT+CMGD=".$matches[0][2];
 	LogMessage("Read command is: ".$readCommand);
 	LogMessage("Delete command is: ".$deleteCommand);
-	SIM868SMSv2_SendCommand(moduleInstanceId, "AT+CMGF=1");
-	$smsMessage = SIM868SMSv2_SendCommand(moduleInstanceId, $readCommand);
-	SIM868SMSv2_SendCommand(moduleInstanceId, $deleteCommand);
+	SIM868SMSv2_SendCommand($moduleInstanceId, "AT+CMGF=1");
+	$smsMessage = SIM868SMSv2_SendCommand($moduleInstanceId, $readCommand);
+	SIM868SMSv2_SendCommand($moduleInstanceId, $deleteCommand);
 	
 	LogMessage("Retrieved the SMS: ". $smsMessage);
 } else
