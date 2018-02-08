7f14720fb9a135a4b3ffc7d8c7348641cb14e098
stigkristiansen <stigkristiansen@outlook.com>
2018-02-08 16:40:02 +0100

    .

--- gateway/module.php
+++ gateway/module.php
@@ -52,7 +52,7 @@ class SIM868GatewayV2 extends IPSModule
 								     array("pattern" => "/^\r\nNORMAL POWER DOWN\r\n$/","forward" => false),
 								     array("pattern" => "/^AT\+CMGR=(\d{1,2})\r\r\n\+CMGR: \"REC.+\",\"(.+)\",\"\",\".+\"\r\n(.+)\r\n\r\nOK\r\n$/i","forward" => false),
 									 array("pattern" => "/\r\nOK\r\n$/","forward" => false),
-									 array("pattern" => "/^AT\+CMGS=\".+\"\r\r\n>$/","forward" => false)
+									 array("pattern" => "/^AT\+CMGS=\".+\".+>$/s","forward" => false)
 									);
 							
 		$log->LogMessage("Searching for complete message...");
